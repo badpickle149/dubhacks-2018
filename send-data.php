f50a079e1b7578095f36990976c6c43b62c10194
Kevin Weng <kevinweng95@gmail.com>
2018-10-21 00:43:19 -0700

    added send-data.php

---
+++ send-data.php
@@ -0,0 +1,36 @@
+<?php
+include_once("cec-utilities.php");
+
+$db = dbInit();
+
+$err_upTmpName = "course_data.csv";
+$row = 0;
+
+if (($handle = fopen($err_upTmpName, "r")) !== FALSE) {
+
+  while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+
+		if($row == 0){
+			$row++;
+		} else {
+
+			// $data[0] = first name; $data[1] = last name; $data[2] = email; $data[3] = phone - remember to change to appropriate fields
+			/*********************************************************************************************************************/
+			if() {
+        
+      }
+
+		}
+
+	}
+
+} else {
+
+	echo 'File could not be opened.';
+}
+
+fclose($handle);
+
+
+
+ ?>
